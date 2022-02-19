f9620c9a3419bed4e1c97889d74f24e115af40e0
LucasLimaPE <robertolucas011@gmail.com>
2022-02-19 11:35:28 -0300

    Adiciona requisito 2.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 /* Iniciando Projeto */
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
       </div>
     );
   }
