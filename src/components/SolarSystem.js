692cbf775a45523c9e8cddeb74ed798d0b69ed9f
LucasLimaPE <robertolucas011@gmail.com>
2022-02-19 20:57:54 -0300

    Adiciona requisito 6 e concerta requisito 5.

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { Planets.map((planets, index) => (<PlanetCard
+          key={ index }
+          planetName={ planets.name }
+          planetImage={ planets.image }
+        />
+        ))}
       </div>
     );
   }
