8be6b809fc623731998472151276ae5ead9ab32e
LucasLimaPE <robertolucas011@gmail.com>
2023-05-27 09:22:02 -0300

    feat: starting application css.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-/* Iniciando Projeto */
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import './reset.css';
+import './App.css';
 
 class App extends React.Component {
   render() {
