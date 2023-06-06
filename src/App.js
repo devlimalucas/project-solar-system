12f3f54302f5d742faa76c4048b1133f15562521
LucasLimaPE <robertolucas011@gmail.com>
2023-06-06 14:35:59 -0300

    style: add the Josh Comeau reset css.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 import Missions from './components/Missions';
+import './reset.css';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div id="root">
       <Header />
       <SolarSystem />
       <Missions />
